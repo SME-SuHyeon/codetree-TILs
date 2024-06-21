b2df10c482d5dd3ce5f7a5a62adbda170c8f2b43
SME-SuHyeon <133241316+SME-SuHyeon@users.noreply.github.com>
2024-06-21 15:43:20 +0900

    Title: 순서를 바꾸었을 때 같은 단어 그룹화하기, Time: 133ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240621/순서를 바꾸었을 때 같은 단어 그룹화하기/group-same-word.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+unordered_map<int, int> hash_map[1005];
+int n;
+int cnt[1005],ans;
+
+int main()
+{
+	cin >> n;
+	
+	string str;
+
+	for (int i = 0; i < n; i++)
+	{
+		cnt[i] = 1;
+
+		cin >> str;
+		for (int s = 0; s < str.length(); s++)
+			hash_map[i][str[s] - 'a']++;
+	}
+
+	unordered_map<int, int>::iterator iter;
+	int mode;
+	for (int x = 0; x < n; x++)
+	{
+		for (int y = x + 1; y < n; y++)
+		{
+			mode = 0;
+			for (iter = hash_map[x].begin(); iter != hash_map[x].end(); iter++)
+			{
+				if (iter->second != hash_map[y][iter->first])
+					mode = 1;
+			}
+
+			if (mode == 0)//같은거라면
+			{
+				cnt[y] += cnt[x];
+
+				ans = max(ans, cnt[y]);
+
+				x++;
+				y = x + 1;
+			}
+		}
+	}
+
+	cout << ans;
+
+
+
+	return 0;
+}
