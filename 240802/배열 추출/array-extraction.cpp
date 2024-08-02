a59537345a774bddee2c0031c70c101da2bb70c9
SME-SuHyeon <133241316+SME-SuHyeon@users.noreply.github.com>
2024-08-02 14:40:45 +0900

    Title: 배열 추출, Time: 148ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 240802/배열 추출/array-extraction.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <tuple>
+#include <queue>
+using namespace std;
+
+
+priority_queue <int> pq;
+int n;
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+
+		cin >> x;
+
+		if (x == 0)
+		{
+			if (pq.empty() == true)
+				cout << "0\n";
+			else
+			{
+				cout << pq.top() << '\n';
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(x);
+		}
+	}
+	
+	
+	return 0;
+}
