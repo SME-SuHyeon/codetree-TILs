e82c0c8be120fe7d8fe8aeb5cb3e9391588b173a
SME-SuHyeon <133241316+SME-SuHyeon@users.noreply.github.com>
2024-09-03 11:17:55 +0900

    Title: 정원 입장은 선착순, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240903/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
+++ 240903/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.cpp
@@ -1,7 +1,68 @@
 #include <iostream>
+#include <set>
+#include <string>
+#include <tuple>
+#include <queue>
 using namespace std;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    return 0;
+priority_queue <pair<int, int>> pq;
+priority_queue <pair<int, int>> waiting;
+
+int n;
+int garden_time[10005];
+int waited_time[10005];
+
+int cur_time = 0;
+
+int main()
+{
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int a, t;
+		cin >> a >> t;
+		
+		garden_time[i] = t;
+		pq.push(make_pair(-a, -i));
+	}
+
+	pair<int, int> top,tmp;
+
+	while (pq.empty() == false)
+	{
+		top = pq.top();
+
+		if (waiting.empty() == true||(cur_time>=-top.first))
+		{
+			tmp.first = top.second;
+			tmp.second = top.first;
+
+			waiting.push(tmp);
+			pq.pop();
+
+			cur_time = max(cur_time, -top.first);
+		}
+		else
+		{
+			tmp = waiting.top();
+			waiting.pop();
+
+			waited_time[-tmp.first] = cur_time - (-tmp.second);
+
+			cur_time += garden_time[-tmp.first];
+		}
+	}
+
+	int maximum = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		if (maximum < waited_time[i])
+			maximum = waited_time[i];
+	}
+
+	cout << maximum;
+
+
+	return 0;
 }
